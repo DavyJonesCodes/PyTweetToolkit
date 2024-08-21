a4f8bbf2479355d0d4eaf001d3f5f75d34dd5036
Dev Jones <devjonescodes@gmail.com>
2024-08-21 11:01:39 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name="PyTweetToolkit",
-    version="1.0.0",
+    version="1.0.1",
     author='Dev Jones',
     author_email='yashdhankhar5656@gmail.com',
     description='PyTweetToolkit: An intuitive Python library for managing Twitter interactions, providing tools for posting tweets, engaging with users, and analyzing social media metrics. Perfect for automating tasks and integrating Twitter functionality into Python projects.',  # Short project description
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/DavyJonesCodes/PyTweetToolkit',
     packages=find_packages(),
